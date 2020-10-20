0d208c320208713cb8be28ffd9086e12feb4cb3a
Simon Nishi McCorkindale <simon@funx.jp>
2020-10-21 08:07:07 +0900

    Import the beginings of the race scene with C#->C++ calling of MeterFeed library (to get entropy from the QRNGs)

---
+++ Assets/Scripts/MeterFeederBridge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeterFeederBridge : MonoBehaviour
+{
+    [DllImport("meterfeeder")]
+    private static extern int Initialize(out IntPtr err);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IntPtr err;
+        int res = Initialize(out err);
+        var hello = Marshal.PtrToStringAnsi(err);
+        Debug.Log($"res: {res}, {hello}");
+    }
+}
