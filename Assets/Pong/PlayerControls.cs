7daab234efa7b25c63fd53835dc5657408c94849
Simon Nishi McCorkindale <simon@funx.jp>
2021-04-24 21:54:40 +0800

    Merge in MED Pong project

---
+++ Assets/Pong/PlayerControls.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class PlayerControls : MonoBehaviour {
+
+	public KeyCode moveUp = KeyCode.W;
+	public KeyCode moveDown = KeyCode.S;
+    public KeyCode qrngToggle = KeyCode.Q;
+	public float speed = 10.0f;
+	public float boundY = 2.25f;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+
+		MedStart();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// var vel = rb2d.velocity;
+		// if (Input.GetKey (moveUp)) {
+		// 	vel.y = speed;
+		// } else if (Input.GetKey (moveDown)) {
+		// 	vel.y = -speed;
+		// } else if (!Input.anyKey) {
+		// 	vel.y = 0;
+		// }
+		// rb2d.velocity = vel;
+
+        var vel = rb2d.velocity;
+        vel = ReadInputAndMove(vel);
+		rb2d.velocity = vel;
+
+		var pos = transform.position;
+		if (pos.y > boundY) {
+			pos.y = boundY;
+		} else if (pos.y < -boundY) {
+			pos.y = -boundY;
+		}
+		transform.position = pos;
+	}
+
+	//---------------- MEDiness
+
+	public string MedDevice = "QWR4E001";
+
+	private static readonly int MF_ERROR_STR_MAX_LEN = 256;
+    private static StringBuilder sMFErrorReason = new StringBuilder(MF_ERROR_STR_MAX_LEN);
+    
+    [DllImport("meterfeeder", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void MF_GetBytes(int length, IntPtr buffer, string generatorSerialNumber, StringBuilder pErrorReason);
+    [DllImport("meterfeeder", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr MF_GetByte(string generatorSerialNumber, StringBuilder pErrorReason);
+
+    void MedStart()
+    {
+        StartCoroutine(ReadMed());
+    }
+
+    IEnumerator ReadMed()
+    {
+        while (true)
+        {
+            IntPtr bufferPtr = Marshal.AllocCoTaskMem(len);
+            MF_GetBytes(len, bufferPtr, MedDevice, sMFErrorReason);
+            byte[] buffer = new byte[len];
+            Marshal.Copy(bufferPtr, buffer, 0, len);
+            num0s = num1s = 0;
+            for (int i = 0; i < len; i++)
+            {
+                int sb = countSetBits(buffer[i]);
+                num1s += sb;
+                num0s += (8 - sb);
+            }
+            yield return null;
+        }
+    }
+
+    public bool qrngOn = true;
+    private float waitTime = 0.2f;
+    private float timer = 0.0f;
+    int len = 256;
+    int num1s = 0, num0s = 0;
+
+    Vector2 ReadInputAndMove(Vector2 vel)
+    {
+        // get the next direction from keyboard
+        if (Input.GetKeyDown(qrngToggle))
+        {
+            qrngOn = !qrngOn;
+            Debug.Log(qrngOn ? gameObject.name + ": Random walking" : "Press WS/↑↓ buttons to walk");
+        }
+
+        timer += Time.deltaTime;
+
+        if (qrngOn)
+        {
+            if (num1s > num0s)
+            {
+                vel.y = -speed;
+                Debug.Log($"1: num1s:{num1s}, num0s:{num0s}");
+            }
+            else if (num1s == num0s)
+            {
+                Debug.LogError("SAME NUMBER OF BITS!!!");
+                Debug.Log($"2: num1s:{num1s}, num0s:{num0s}");
+                vel.y = 0;
+            }
+            else
+            {
+                vel.y = speed;
+                Debug.Log($"3: num1s:{num1s}, num0s:{num0s}");
+            }
+
+            timer = timer - waitTime;
+        }
+        else
+        {
+            // Normal up/down logic
+            if (Input.GetKey (moveUp)) {
+                vel.y = speed;
+            } else if (Input.GetKey (moveDown)) {
+                vel.y = -speed;
+            } else if (!Input.anyKey) {
+                vel.y = 0;
+            }
+        }
+
+        return vel;
+    }
+
+    int countSetBits(int n)
+    {
+        int count = 0;
+        while (n > 0)
+        {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+}
---
+++ Assets/Pong/PongGameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class PongGameManager : MonoBehaviour {
+
+	public static int PlayerScore1 = 0;
+	public static int PlayerScore2 = 0;
+
+	public GUISkin layout;
+
+	GameObject theBall;
+
+	// Use this for initialization
+	void Start () {
+		theBall = GameObject.FindGameObjectWithTag ("Ball");
+
+		InitMed();
+	}
+
+	public static void Score(string wallID) {
+		if (wallID == "RightWall") {
+			PlayerScore1++;
+		} else {
+			PlayerScore2++;
+		}
+	}
+
+	void OnGUI() {
+		GUI.skin = layout;
+		GUI.Label (new Rect (Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
+		GUI.Label (new Rect (Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
+
+		if (GUI.Button (new Rect (Screen.width / 2 - 60, 35, 120, 53), "RESTART")) {
+			PlayerScore1 = 0;
+			PlayerScore2 = 0;
+			theBall.SendMessage ("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
+		}
+
+		if (PlayerScore1 == 10) {
+			GUI.Label (new Rect (Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
+			theBall.SendMessage ("ResetBall", null, SendMessageOptions.RequireReceiver);
+		} else if (PlayerScore2 == 10) {
+			GUI.Label (new Rect (Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
+			theBall.SendMessage ("ResetBall", null, SendMessageOptions.RequireReceiver);
+		}
+	}
+
+	private static readonly int MF_ERROR_STR_MAX_LEN = 256;
+    private static StringBuilder sMFErrorReason = new StringBuilder(MF_ERROR_STR_MAX_LEN);
+
+    [DllImport("meterfeeder", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int MF_Initialize(StringBuilder pErrorReason);
+    [DllImport("meterfeeder", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int MF_Shutdown();
+
+	void InitMed()
+    {
+        int medRes = -1;
+        sMFErrorReason.Clear();
+        medRes = MF_Initialize(sMFErrorReason);
+        Debug.Log($"MeterFeeder MF_Initialize: result:{medRes}, errorReason:{sMFErrorReason}");
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("MeterFeeder MF_Shutdown");
+        MF_Shutdown();
+    }
+}
